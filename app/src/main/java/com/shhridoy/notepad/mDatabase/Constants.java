f248df5fa34822782b7f22f12813d90bebf2ab52
shhridoy <shhridoy.cste@gmail.com>
2018-07-28 03:13:31 +0600

    Initial commit

---
+++ app/src/main/java/com/shhridoy/notepad/mDatabase/Constants.java
@@ -0,0 +1,35 @@
+package com.shhridoy.notepad.mDatabase;
+
+/**
+ * Created by whoami on 7/28/2018.
+ */
+
+public class Constants {
+
+    public static final String DB_NAME = "notepad.db";
+    public static final int DB_VERSION = 1;
+
+    // NOTES TABLE CONSTANTS
+    public static final String NOTES_TABLE = "notes";
+
+    public static final String NOTES_ID = "notes_id";
+    public static final String NOTES_TITLE = "notes_title";
+    public static final String NOTES_DETAILS = "notes_details";
+    public static final String NOTES_DATE_TIME = "notes_date_time";
+    public static final String NOTES_PASSWORD = "notes_password";
+    public static final String NOTES_PASS_HINT = "notes_pass_hint";
+    public static final String NOTES_COLOR = "notes_color";
+
+    public static final String CREATE_NOTES_TABLE = "CREATE TABLE "+ NOTES_TABLE +
+            "( " +
+            NOTES_ID +" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            NOTES_TITLE +" TEXT, " +
+            NOTES_DETAILS +" TEXT, " +
+            NOTES_DATE_TIME + " DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+            NOTES_PASSWORD + " TEXT, " +
+            NOTES_PASS_HINT + " TEXT, " +
+            NOTES_COLOR + " TEXT);";
+
+    public static final String DROP_NOTES_TABLE = "DROP TABLE IF EXISTS " + NOTES_TABLE;
+
+}
---
+++ app/src/main/java/com/shhridoy/notepad/mDatabase/DatabaseHelper.java
@@ -0,0 +1,69 @@
+package com.shhridoy.notepad.mDatabase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by whoami on 7/28/2018.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(Context context) {
+        super(context, Constants.DB_NAME, null, Constants.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Constants.CREATE_NOTES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(Constants.DROP_NOTES_TABLE);
+        onCreate(db);
+    }
+
+    public void addNote(String title, String details, String password, String password_hint, String color) {
+
+        ContentValues cv = new ContentValues();
+        cv.put(Constants.NOTES_TITLE, title);
+        cv.put(Constants.NOTES_DETAILS, details);
+        cv.put(Constants.NOTES_PASSWORD, password);
+        cv.put(Constants.NOTES_PASS_HINT, password_hint);
+        cv.put(Constants.NOTES_COLOR, color);
+        this.getWritableDatabase().insertOrThrow(Constants.NOTES_TABLE, "", cv);
+        this.getWritableDatabase().close();
+
+    }
+
+    public boolean editNote(int id, String title, String details, String password, String password_hint, String color) {
+        ContentValues cv = new ContentValues();
+        cv.put(Constants.NOTES_TITLE, title);
+        cv.put(Constants.NOTES_DETAILS, details);
+        cv.put(Constants.NOTES_PASSWORD, password);
+        cv.put(Constants.NOTES_PASS_HINT, password_hint);
+        cv.put(Constants.NOTES_COLOR, color);
+
+        int result = this.getWritableDatabase()
+                .update(Constants.NOTES_TABLE, cv, Constants.NOTES_ID+" =?", new String[]{String.valueOf(id)});
+        this.getWritableDatabase().close();
+        return result > 0;
+    }
+
+    public Cursor getNotes() {
+        Cursor cursor = this.getReadableDatabase().rawQuery("SELECT * FROM "+Constants.NOTES_TABLE, null);
+        this.getReadableDatabase().close();
+        return cursor;
+    }
+
+    public boolean removeNote(int id) {
+        int result = this.getWritableDatabase().delete(Constants.NOTES_TABLE, Constants.NOTES_ID+" = "+id, null);
+        this.getWritableDatabase().close();
+        return result > 0;
+    }
+
+}
