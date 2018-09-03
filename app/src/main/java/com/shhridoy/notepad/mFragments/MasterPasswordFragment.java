65eafccc6ccad354b4346ee25ffa90fcf77472da
shhridoy <shhridoy.cste@gmail.com>
2018-09-04 00:23:34 +0600

    f;ajfa

--- app/src/main/java/com/shhridoy/notepad/mFragments/MasterPasswordFragment.java
+++ app/src/main/java/com/shhridoy/notepad/mFragments/MasterPasswordFragment.java
@@ -1,8 +1,9 @@
 package com.shhridoy.notepad.mFragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,8 +13,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shhridoy.notepad.R;
+import com.shhridoy.notepad.mUtilities.MyPreferences;
 
 /**
  * Created by whoami on 8/18/2018.
@@ -21,17 +24,34 @@ import com.shhridoy.notepad.R;
 
 public class MasterPasswordFragment extends Fragment {
 
-    private TextView currentPasswordTitleTV, newPasswordTitleTV, userHintTV;
+    private TextView currentPasswordTitleTV, newPasswordTitleTV, userHintTV, currentPasswordWarningTV, newPasswordWarningTV;
     private EditText currentPasswordET, newPasswordET1, newPasswordET2;
     private Button saveButton;
 
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.master_password_fragment, container, false);
 
         initViews(rootView);
+
+        if (!MyPreferences.getPreference(getContext(), "Password").equals("Default")) {
+            currentPasswordTitleTV.setVisibility(View.GONE);
+            currentPasswordET.setVisibility(View.GONE);
+        }
+
+        currentPasswordWarningTV.setVisibility(View.GONE);
+        newPasswordWarningTV.setVisibility(View.GONE);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentPasswordET.getText().toString().trim().length() == 0) {
+                    Toast.makeText(getContext(), "Enter you current password!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         return rootView;
     }
@@ -55,6 +75,8 @@ public class MasterPasswordFragment extends Fragment {
         currentPasswordTitleTV = v.findViewById(R.id.masterPasswordTitle1TV);
         newPasswordTitleTV = v.findViewById(R.id.masterPasswordTitle2TV);
         userHintTV = v.findViewById(R.id.masterPasswordUserHintTV);
+        currentPasswordWarningTV = v.findViewById(R.id.masterPasswordCurrentWarningTV);
+        newPasswordWarningTV = v.findViewById(R.id.masterPasswordNewWarningTV);
 
         currentPasswordET = v.findViewById(R.id.masterPasswordETCurrent);
         newPasswordET1 = v.findViewById(R.id.masterPasswordET1);
